f8e40b57d685ed373c06d99574ab30bb75dc15f4
Jhoana Espana <jhoespana@gmail.com>
2021-07-05 13:41:59 -0500

    subi el ejercicio 2 del curso

--- README.md
+++ README.md
@@ -9,3 +9,10 @@ Cuso practico de la escuela de Desarrollo Web de Platzi
 - Segundo paso: implementar las formulas en JavaScript
 - Tercer paso: crear funciones
 - Cuarto paso: integrar JavaScript con HTML
+
+## Taller #2: Porcentajes y descuentos
+
+- Primer paso: definir las formulas
+- Segundo paso: implementar las formulas en JavaScript
+- Tercer paso: crear funciones
+- Cuarto paso: integrar JavaScript con HTML
--- isoceles.js
+++ isoceles.js
@@ -11,3 +11,23 @@ function alturaTrianguloIsosceles(lado1, lado2, base){
 }
 
 console.log(alturaTrianguloIsoceles(5, 5, 8));
+
+
+// Codigo del profe
+
+function alturaTrianguloIsosceles(trianguloGrandeLadoA, trianguloGrandeLadoB, trianguloGrandeLadoBase) {
+  if (trianguloGrandeLadoA != trianguloGrandeLadoB) {
+      console.error("Los lados a y b no son iguales");
+  } else {
+      const trianguloPequenoLadoB = trianguloGrandeLadoBase / 2;
+      const trianguloPequenoLadoBase = trianguloGrandeLadoA;
+
+      const trianguloPequenoLadoBCuadrado = trianguloPequenoLadoB * trianguloPequenoLadoB;
+      const trianguloPequenoLadoBaseCuadrado = trianguloPequenoLadoBase * trianguloPequenoLadoBase;
+
+      const trianguloPequenoLadoA = Math.sqrt(trianguloPequenoLadoBaseCuadrado - trianguloPequenoLadoBCuadrado);
+
+      const trianguloGrandeAltura = trianguloPequenoLadoA;
+      return trianguloGrandeAltura;
+  }
+}
